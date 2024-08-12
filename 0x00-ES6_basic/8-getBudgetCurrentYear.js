d26360610b8748f5773f78e41acf2b4ee1656f10
rasaq-ola <rasaqola196@gmail.com>
2024-08-12 18:14:33 +0100

    ES6 Basics

---
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,12 @@
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
+}
+
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
