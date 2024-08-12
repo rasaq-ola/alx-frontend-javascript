36676be1492627e23ed003ab1fb44f2586f6ed11
rasaq-ola <rasaqola196@gmail.com>
2024-08-12 17:30:32 +0100

    Frontend javascript

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,12 @@
+export default function cleanSet(set, startString) {
+  if (!startString || typeof startString !== 'string') return '';
+  
+  let result = '';
+  set.forEach((value) => {
+    if (value.startsWith(startString)) {
+      result += `${value.slice(startString.length)}-`;
+    }
+  });
+  
+  return result.slice(0, -1);
+}
